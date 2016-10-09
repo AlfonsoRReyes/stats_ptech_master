6b7b7689ef1f10ea6e0147ae0170988e8a745440
ipm wint7x64-ipm10 <ipm.wint7x64-ipm10@reyesaguayo.com>
2016-10-09 13:44:31 -0500

    improving setup.py to allow the develop option

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from distutils.core import setup
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
-setup(
-    name='stats_ptech_master',
-    version='0.1',
-    packages=['stats_ptech'],
-    url='',
-    license='MIT',
-    author='alfonso r. reyes',
-    author_email='',
-    description='statistics distributions percentiles'
-)
+
+setup(name='stats_ptech_master',
+      version='0.0.1',
+      description='statistics distributions percentiles.',
+      maintainer='Alfonso R. Reyes',
+      maintainer_email='alfonso.r.reyes@gmail.com',
+      url='https://github.com/AlfonsoRReyes/stats_ptech_master',
+      license='MIT',
+      platforms='any',
+      packages=['stats_ptech'],
+      keywords=['pandas', 'data analysis', 'statistics'],
+      install_requires=[
+          'numpy',
+          'pandas',
+          'matplotlib'
+      ],
+      classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering'
+      ],
+      #tests_require=['pytest'],
+      #cmdclass={'test': PyTest}
+      )
